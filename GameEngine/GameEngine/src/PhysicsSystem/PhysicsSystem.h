e87271d0eeea5d89e5134ded2efd3347e1575bc6
Olof Bourghardt <ob655088@gmail.com>
2021-05-22 09:35:30 +0200

    added some comments

--- GameEngine/GameEngine/src/PhysicsSystem/PhysicsSystem.h
+++ GameEngine/GameEngine/src/PhysicsSystem/PhysicsSystem.h
@@ -2,15 +2,18 @@
 #include "../EntityComponentSystem/System.h"
 #include "Ogre.h"
 
+//! The physics system takes care of applying gracity and velocity to entities with a rigid body
 class PhysicsSystem : public System
 {
 public:
 	// Inherited via System
 	virtual void handleMessage(Message* msg) override;
 
+	//! Applies gravity to all transforms of rigid body components
 	void update(const Ogre::FrameEvent& fe);
 
 private:
+	//! The maximum velocity of an entity
 	const float MAXIMUM_VELOCITY = 3.0;
 };
 
